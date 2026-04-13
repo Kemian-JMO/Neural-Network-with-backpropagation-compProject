87326429752e5eb674027904fbd5a09b9455608b
jonas <Jonas@olsson-web.dk>
2026-04-13 16:15:08 +0200

    Refactor NeuralNetwork: organize setup logic by extracting neuron initialization steps into dedicated methods (setPrevNeuron, setWeights, setBias).

--- src/main/java/com/ann/project/NeuralNetwork.java
+++ src/main/java/com/ann/project/NeuralNetwork.java
@@ -29,6 +29,27 @@ public class NeuralNetwork{
     the output values are objects, as we don't know what the output will be at this point.
     this could maybe be Strings?
      */
+
+    /*
+    The neural network is set up, now we need to begin to make the training logic
+    feedforward:
+    - set the input values as the activation values for the input neurons
+    - calculate the activation values for the hidden neurons one layer at a time
+    - this is done by multiplying the weights of the next layer by the activation values of the previous layer
+    - by making a matrix multiplication of the weights and the activation values of the previous layer we can calculate the activation values of the next layer at once
+    - then the bias is added by adding the bias to the product of the weights and the activation values of the previous layer in a matrix addition
+    - the activation function is applied to the result of the matrix addition
+        * should we have multiple activation functions and choose one?
+        * it wouldnt be too hard to implement and we could have more to discuss about our results in the report
+        * I have mostly read about Relu and sigmoid, for what i could read Relu should be better for object identification
+    - calculate the activation values for the output neurons
+    - compare the output values to the desired output values
+    backpropagation:
+    - calculate the error for each output neuron
+    - calculate the error for each hidden neuron
+    - update the weights and biases for each neuron
+
+     */
     private ArrayList<Object> outputValues = new ArrayList<>();
 
     public NeuralNetwork(int seed, int[] hiddenLayers, ArrayList<Double[]> inputValues, ArrayList<Object> outputValues) {
@@ -44,26 +65,10 @@ public class NeuralNetwork{
         network.addAll(hiddenNeurons);
         network.add(outputNeurons);
 
-        /*
-        This is the part where we set the prevNeurons of each neuron.
-         */
-        for (int i = 1; i < network.size(); i++) {
-            for (int j = 0; j < network.get(i).size(); j++) {
-                network.get(i).get(j).setPrevNeurons(network.get(i - 1));
-            }
-        }
+        setPrevNeuron();
+        setWeights();
+        setBias();
 
-        /*
-        This is the part where we set the weights of each neuron.
-         */
-
-        /*
-        This is the part where we set the bias of each neuron.
-         */
-
-        /*
-        This is the part where we set the activation function of each neuron in the first layer.
-         */
     }
 
     private void createHiddenNeurons(){
@@ -92,11 +97,33 @@ public class NeuralNetwork{
         }
     }
 
-    private ArrayList<Double> populateWeights(int Size){
-        ArrayList<Double> weights = new ArrayList<>();
-        for(int i = 0; i < Size; i++){
-            weights.add(random.nextDouble());
+    private void setPrevNeuron(){
+        for (int i = 1; i < network.size(); i++) {
+            for (int j = 0; j < network.get(i).size(); j++) {
+                network.get(i).get(j).setPrevNeurons(network.get(i - 1));
+            }
         }
-        return weights;
     }
+
+    private void setWeights(){
+        for(ArrayList<Neuron> neurons : network){
+            for(Neuron neuron : neurons){
+                ArrayList<Double> weights = new ArrayList<>();
+                for(int i = 0; i < neuron.getPrevNeurons().size(); i++){
+                    weights.add(random.nextDouble());
+                }
+                neuron.setWeight(weights);
+            }
+        }
+    }
+
+    private void setBias(){
+        for(ArrayList<Neuron> neurons : network){
+            for(Neuron neuron : neurons){
+                neuron.setBias(random.nextDouble());
+            }
+        }
+    }
+
+
 }
