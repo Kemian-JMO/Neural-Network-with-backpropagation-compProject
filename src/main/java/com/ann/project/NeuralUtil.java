38607f932ca7a8f091d7daf025f7d04b460c43fc
jonas <Jonas@olsson-web.dk>
2026-05-04 21:39:18 +0200

    Add Xavier and He initialization to `NeuralUtil`, improve dot product validation

--- src/main/java/com/ann/project/NeuralUtil.java
+++ src/main/java/com/ann/project/NeuralUtil.java
@@ -1,5 +1,7 @@
 package com.ann.project;
 
+import java.util.Random;
+
 public final class NeuralUtil {
 
     private NeuralUtil(){}
@@ -9,14 +11,30 @@ public final class NeuralUtil {
     }
 
     public static double[][] dotMatrix(double[][] a, double[][] b){
+
         double[][] result = new double[a.length][b[0].length];
         for(int i = 0; i < a.length; i++){
             for(int j = 0; j < b[0].length; j++){
                 for(int k = 0; k < a[0].length; k++){
+                    if (a[0].length != b.length) {
+                        throw new IllegalArgumentException("Arrays have different lengths");
+                    }
                     result[i][j] += a[i][k] * b[k][j];
                 }
             }
         }
         return result;
     }
+
+    public static double xavierUniform(int fanIn, int fanOut, Random r){
+        double limit = Math.sqrt((double) 6 / (fanIn + fanOut));
+        return (r.nextDouble() * 2 - 1) * limit;
+    }
+
+    public static double heUniform(int fanIn, Random r){
+        double limit = Math.sqrt((double) 6 / fanIn);
+        return (r.nextDouble() * 2 - 1) * limit;
+    }
 }
+
+
