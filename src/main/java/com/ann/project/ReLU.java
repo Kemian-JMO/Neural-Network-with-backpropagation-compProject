e6ef9a66268f725ca849f87bd0008cab091ad8e1
jonas <Jonas@olsson-web.dk>
2026-05-04 21:39:39 +0200

    Add `ReLU` implementation: skeleton class for testing and interface compliance

---
+++ src/main/java/com/ann/project/ReLU.java
@@ -0,0 +1,17 @@
+package com.ann.project;
+
+/*
+does not work but was needed for the testing to work
+ */
+public class ReLU implements Activation{
+
+    @Override
+    public double[] apply(double[][] Z) {
+        return new double[0];
+    }
+
+    @Override
+    public double[] derivative(double[][] Z, double[][] A) {
+        return new double[0];
+    }
+}
