0182ea4cf2eca21208ece23b21c783b77a6d793b
KemKillian <55388741+Kemian-JMO@users.noreply.github.com>
2026-05-05 12:23:44 +0200

    Added bias handling and activation computation in `feedForward` method.

--- src/main/java/com/ann/project/NeuralUtil.java
+++ src/main/java/com/ann/project/NeuralUtil.java
@@ -6,10 +6,6 @@ public final class NeuralUtil {
 
     private NeuralUtil(){}
 
-    public static double sigmoid(double x){
-        return 1/(1+Math.exp(-x));
-    }
-
     public static double[][] dotMatrix(double[][] a, double[][] b){
 
         double[][] result = new double[a.length][b[0].length];
@@ -21,6 +17,16 @@ public final class NeuralUtil {
                     }
                     result[i][j] += a[i][k] * b[k][j];
                 }
+            }
+        }
+        return result;
+    }
+
+    public static double[][] matrixAddBias(double[][] matrix, double bias[]){
+        double[][] result = new double[matrix.length][matrix[0].length + 1];
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                result[i][j] = matrix[i][j] + bias[i];
             }
         }
         return result;
