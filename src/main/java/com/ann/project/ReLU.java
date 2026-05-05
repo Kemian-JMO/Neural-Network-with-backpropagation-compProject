13c4574fe6f667ad5d7461b4be05fbabb6c8c2d8
KemKillian <55388741+Kemian-JMO@users.noreply.github.com>
2026-05-05 12:23:54 +0200

    Updated `ReLU` to use 2D arrays for `apply` and `derivative` methods.

--- src/main/java/com/ann/project/ReLU.java
+++ src/main/java/com/ann/project/ReLU.java
@@ -6,12 +6,12 @@ does not work but was needed for the testing to work
 public class ReLU implements Activation{
 
     @Override
-    public double[] apply(double[][] Z) {
-        return new double[0];
+    public double[][] apply(double[][] Z) {
+        return new double[0][];
     }
 
     @Override
-    public double[] derivative(double[][] Z, double[][] A) {
-        return new double[0];
+    public double[][] derivative(double[][] Z, double[][] A) {
+        return new double[0][];
     }
 }
