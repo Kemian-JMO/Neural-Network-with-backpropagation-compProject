d8ce50bc4d0b3295bf2ff606dbebfd07b370ea3a
KemKillian <55388741+Kemian-JMO@users.noreply.github.com>
2026-05-11 16:49:56 +0200

    Integrated basic neural network setup with ReLU and SoftMax activations and added batch training capability in `Main.java`.

--- src/main/java/com/ann/project/Main.java
+++ src/main/java/com/ann/project/Main.java
@@ -7,7 +7,16 @@ public class Main {
         System.out.println("Indlæser MNIST data...");
 
         double[][] images = MnistLoader.loadImages(MnistLoader.TRAIN_IMAGE_FILE);
-        int[]      labels = MnistLoader.loadLabels(MnistLoader.TRAIN_LABEL_FILE);
+        double[][]      labels = MnistLoader.loadLabels(MnistLoader.TRAIN_LABEL_FILE);
+        System.out.println(images[0].length);
+
+        Activation RELU = new ReLU();
+        Activation SOFTMAX = new SoftMax();
+        Activation[] activations = {null, RELU, SOFTMAX};
+        int[] hiddenLayers = {16};
+
+        NeuralNetwork neuralNetwork = new NeuralNetwork(10,0.01, activations,10, hiddenLayers, images, labels);
+        neuralNetwork.trainBatch();
 
         System.out.println("Billeder indlæst: " + images.length);
         System.out.println("Labels indlæst: "   + labels.length);
