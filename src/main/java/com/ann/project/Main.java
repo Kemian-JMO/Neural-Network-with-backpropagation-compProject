1728e46de75fd12dea7106ad99d0cd967d1b35ec
jonas <Jonas@olsson-web.dk>
2026-05-18 18:20:06 +0200

    Refactor `Main`: enhance model configuration, add validation and inference support

--- src/main/java/com/ann/project/Main.java
+++ src/main/java/com/ann/project/Main.java
@@ -8,20 +8,26 @@ public class Main {
 
         double[][] images = MnistLoader.loadImages(MnistLoader.TRAIN_IMAGE_FILE);
         double[][] labels = MnistLoader.loadLabels(MnistLoader.TRAIN_LABEL_FILE);
-        double[][] testImages = MnistLoader.loadImages(MnistLoader.TEST_IMAGE_FILE);
-        double[][] testLabels = MnistLoader.loadLabels(MnistLoader.TEST_LABEL_FILE);
-        double[][][] data = {images, labels, testImages, testLabels};
+        double[][] valImages = MnistLoader.loadImages(MnistLoader.TEST_IMAGE_FILE);
+        double[][] valLabels = MnistLoader.loadLabels(MnistLoader.TEST_LABEL_FILE);
+        double[][][] data = {images, labels, valImages, valLabels};
         System.out.println(images[0].length);
+
 
         Activation RelU = new ReLU();
         Activation SOFTMAX = new SoftMax();
-        Activation[] activations = {null, RelU, RelU, SOFTMAX};
-        int[] hiddenLayers = {20,20};
+        Activation[] activations = {null, RelU, RelU, RelU, SOFTMAX};
+        int[] hiddenLayers = {200,200,200};
 
         NeuralNetwork neuralNetwork = new NeuralNetwork(10,10,30,0.01, activations, hiddenLayers, data);
         neuralNetwork.trainEpoch();
 
+
+        Imager img = new Imager();
+        double[][] testImages = img.getImageData();
+        double[][] testLabels = img.getImageLabel();
         NeuralNetwork bestNetwork = new NeuralNetwork(NeuralNetwork.loadNetwork("Models/network.nn"));
+        bestNetwork.runInferenceSingle(testImages, testLabels);
         System.out.println("Billeder indlæst: " + images.length);
         System.out.println("Labels indlæst: "   + labels.length);
         System.out.println("Første label: "     + labels[0]);
