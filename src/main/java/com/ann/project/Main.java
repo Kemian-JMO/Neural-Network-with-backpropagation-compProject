d9f6c0774a90c8a0b25e411ca8d92e9a60375132
jonas <Jonas@olsson-web.dk>
2026-05-22 21:16:16 +0200

    Add `Charts` class to visualize neural network training metrics

--- src/main/java/com/ann/project/Main.java
+++ src/main/java/com/ann/project/Main.java
@@ -18,17 +18,20 @@ public class Main {
         Activation Sigmoid = new Sigmoid();
         Activation SOFTMAX = new SoftMax();
         Activation[] activations = {null, RelU, RelU, RelU, SOFTMAX};
-        int[] hiddenLayers = {200,200,200};
+        int[] hiddenLayers = {250,250,250};
+        String best= "Models/best_";
+        String name = "L3_N250-250-250_E10_AReLU_Lr0.01.nn";
 
-        NeuralNetwork neuralNetwork = new NeuralNetwork(10,10,30,0.01, activations, hiddenLayers, data, "network.nn", true);
-        //neuralNetwork.trainEpoch();
-
+        NeuralNetwork neuralNetwork = new NeuralNetwork(10,10,30,0.01, activations, hiddenLayers, data, name, true);
+        neuralNetwork.trainEpoch((EpochListener) (epoch, trainLoss, valLoss, accuracy) -> {});
 
         Imager img = new Imager();
         double[][] testImages = img.getImageData();
         double[][] testLabels = img.getImageLabel();
-        NeuralNetwork bestNetwork = new NeuralNetwork(NeuralNetwork.loadNetwork("Models/network.nn"));
+        System.out.println("Network: " + name);
+        NeuralNetwork bestNetwork = new NeuralNetwork(NeuralNetwork.loadNetwork(best + name));
         bestNetwork.runInferenceSingle(testImages, testLabels);
+
         System.out.println("Billeder indlæst: " + images.length);
         System.out.println("Labels indlæst: "   + labels.length);
         System.out.println("Første label: "     + labels[0]);
